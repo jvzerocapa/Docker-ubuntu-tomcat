2caf809f196e61a3944775431dc2cf20c86711d5
João Vitor Gomes Pinheiro Silva <joaovitorgomes503@gmail.com>
2025-10-06 20:33:20 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,11 +15,22 @@ if ! grep -q "username=\"admin\"" "$TOMCAT_USERS_FILE"; then
     cat <<EOL > "$TOMCAT_USERS_FILE"
 <tomcat-users>
   <role rolename="manager-gui"/>
+  <role rolename="manager-script"/>
+  <role rolename="manager-status"/>
   <role rolename="admin-gui"/>
-  <user username="admin" password="admin123" roles="manager-gui,admin-gui"/>
+  <user username="admin" password="admin123" roles="manager-gui,manager-script,manager-status,admin-gui"/>
 </tomcat-users>
 EOL
 fi
+
+# Liberar acesso remoto ao Manager e Host Manager
+for app in manager host-manager; do
+    CONTEXT_FILE="$CATALINA_HOME/webapps/$app/META-INF/context.xml"
+    if [ -f "$CONTEXT_FILE" ]; then
+        echo "Removendo restrição de IP para $app..."
+        sed -i '/Valve className="org.apache.catalina.valves.RemoteAddrValve"/,/\/Valve>/d' "$CONTEXT_FILE"
+    fi
+done
 
 # Iniciar Tomcat em foreground
 exec $CATALINA_HOME/bin/catalina.sh run
