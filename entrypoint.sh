93e24b2b29f9cd96bfc3e2251637a9bba0672262
João Vitor Gomes Pinheiro Silva <joaovitorgomes503@gmail.com>
2025-10-06 17:20:05 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-# Iniciar Tomcat no primeiro plano
+set -e
+
+# DEBUG
+echo "Iniciando Tomcat..."
+echo "CATALINA_HOME = $CATALINA_HOME"
+
+# Garantir permissões corretas
+chmod -R 755 $CATALINA_HOME
+
+# Iniciar Tomcat em foreground
 exec $CATALINA_HOME/bin/catalina.sh run
